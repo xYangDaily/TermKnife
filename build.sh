00070b39dba832f0d00a5f1690975c2052c474fb
xYangDaily <xyang8520@gmail.com>
2025-01-25 23:47:50 +0800

    fix build

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-conf_path="~/.config"
+conf_path="$HOME/.config"
 mkdir -p $conf_path
 
-if [ ! -e $conf_path/nvim ];
-  ln -s nvim $conf_path/nvim
+if [ ! -e $conf_path/nvim ]; then
+  ln -s `pwd`/nvim $conf_path/nvim
 fi
 
-if [ ! -e $conf_path/tmux ];
-  ln -s tmux $conf_path/tmux
-  ln -s tmux/tmux.conf ~/.tmux.conf
+if [ ! -e $conf_path/tmux ]; then
+  ln -s `pwd`/tmux $conf_path/tmux
+  ln -s `pwd`/tmux/tmux.conf ~/.tmux.conf
 fi
 
 chmod -R +x zsh
-if [ ! -e $conf_path/zsh ];
-  ln -s zsh $conf_path/zsh
-  ln -s zsh/zshrc ~/.zshrc
+if [ ! -e $conf_path/zsh ]; then
+  ln -s `pwd`/zsh $conf_path/zsh
+  ln -s `pwd`/zsh/zshrc ~/.zshrc
 fi
